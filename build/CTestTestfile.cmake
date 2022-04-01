954f7a03c1d7db6254c7e3310c282d12cf51d1ac
SweetTaroPie <h372chen@uwaterloo.ca>
2022-04-01 00:20:45 -0400

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/harrischen/projects/helloworld
+# Build directory: /Users/harrischen/projects/helloworld/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
